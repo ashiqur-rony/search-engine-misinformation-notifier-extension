6c0c25dcc0812da4c6cf46fd9f2066c58575a2db
Ashiqur Rahman <ashiquebd@gmail.com>
2020-08-13 00:32:22 -0500

    Updated the URLs of search engines.

--- js/popup.js
+++ js/popup.js
@@ -6,7 +6,7 @@ var url;
 document.addEventListener( 'DOMContentLoaded', function () {
     document.getElementById( 'privacy-advice' ).innerHTML = 'This extension reads the web page for ' +
         'misinformation trigger keywords and sends to an AI model for further analysis. ' +
-        'This extension does not in any way collect any personal or identifiable data. ' +
-        'Please disable the extension if you do not agree to that.';
+        'This extension does not collect any personal identifiable information (PII) in any way. ' +
+        'Please feel free to disable this extension in any point of time if you do not agree with the terms mentioned above.';
     //console.log(chrome);
 } );
